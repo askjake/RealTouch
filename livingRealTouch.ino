c1ba7907944fc39286871cec72154f5a339eb4bd
askjake <96503469+askjake@users.noreply.github.com>
2023-05-19 11:59:20 -0600

    Works at 5/19/2023
    This is maybe the first version to talk to every remote and inputs are not case sensative.

--- livingRealTouch.ino
+++ livingRealTouch.ino
@@ -15,7 +15,7 @@ uint8_t MUX_BUS = 0x01; // Replace with your actual bus number
 uint8_t activePort = 0;
 int remote;
 /////////////////////////////////////
-const int remoteTiming = 2;  //  1 FOR 54.1 REMOTES / 5 FOR 54.3 REMOTES
+const int remoteTiming = 5;  //  1 FOR 54.1 REMOTES / 5 FOR 54.3 REMOTES
 volatile int KEY_RELEASE_TIME = 20;
 volatile byte readMode = 0;
 volatile int receivedValue = 0;
@@ -30,15 +30,22 @@ boolean longholdButton = 0;
 boolean debug = 1;  //debug mode
 boolean initFlag = 0;     // Flag triggers update to CFG_REG initalization string on powerup / reset
 byte location;
+byte CFG_REG;
+
+
+int small_delay = 5;
+int big_delay = 1000;
+
+
 
 // REGISTER VALUES
 byte BLANK = 0x00;
-byte CFG_REG = 0xA7;
 byte INT_REG = 0x00;
 byte EVT_REG = 0x00;
 /////////////////////////////////////
 bool remoteCommandEntered = false;
 void setup() {
+
 
   Wire.begin(ARDUINO_SLAVE_ADDR);  // Initialize I2C communication
   Wire.onReceive(receiveEvent);
@@ -47,23 +54,36 @@ void setup() {
   pinMode(mux_reset, OUTPUT);
   pinMode(rf4ce_reset, OUTPUT);
   digitalWrite(TRIGGER_PIN, HIGH);  // Set the trigger pin HIGH initially
+    delay(500);  
+digitalWrite(TRIGGER_PIN, LOW); //// grab the init 
+  delay(15);    
+  digitalWrite(TRIGGER_PIN, HIGH); 
+  delay(500);  
+
+
   Serial.begin(115200);
   while (!Serial);
   Serial.println("I2C Slave Initialized");
   reset_mux();
+    delay(100);  
   reset_rf4ce();
 
   INT_REG = 0x01;
   EVT_REG = 0x01;
-  selectDefaultBus();  // Select default bus on startup
+  selectPort(MUX_1, 0);  // Select default bus on startup
 
 }
 void loop() {
-
+   if (needRelease == 1){
+	  delay(0);
+      sendrelease();
+   }
   // Check if there is any input available
   if (Serial.available() > 0) {
     String inputString = Serial.readStringUntil('\n');  // read the incoming string
     inputString.trim();                                 // remove any leading/trailing white space
+    inputString.toLowerCase();                          // convert the input to lowercase
+
     if (inputString.startsWith("remote")) {
       remoteCommandEntered = true;
       unsigned int spaceIndex = inputString.indexOf(" ");  // find the index of the space
@@ -134,204 +154,204 @@ void loop() {
       BUTTON = "0";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Diamond") {  //DIAMOND
+    } else if (inputString == "diamond") {  //DIAMOND
       KEY_CMD = 0xC0;
       KEY_RELEASE = 0x40;
       BUTTON = "Diamond";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Double_Diamond") {  // DOUBLE DIAMOND
+    } else if (inputString == "double_diamond") {  // DOUBLE DIAMOND
       KEY_CMD = 0xC2;
       KEY_RELEASE = 0x42;
       BUTTON = "Double_Diamond";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Volume_Up") {  //VOL+
+    } else if (inputString == "volume_up") {  //VOL+
       KEY_CMD = 0xA9;
       KEY_RELEASE = 0x29;
       BUTTON = "Volume_Up";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Volume_Down") {  //VOL-
+    } else if (inputString == "volume_down") {  //VOL-
       KEY_CMD = 0xAC;
       KEY_RELEASE = 0x2C;
       BUTTON = "Volume_Down";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Channel_Up") {  //CH +
+    } else if (inputString == "channel_up") {  //CH +
       KEY_CMD = 0xAB;
       KEY_RELEASE = 0x2B;
-      BUTTON = "Channel_Up";
+      BUTTON = "channel_up";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Channel_Down") {  //CH -
+    } else if (inputString == "channel_down") {  //CH -
       KEY_CMD = 0xAE;
       KEY_RELEASE = 0x2E;
       BUTTON = "Channel_Down";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Recall") {  // RECALL
+    } else if (inputString == "recall") {  // RECALL
       KEY_CMD = 0xAA;
       KEY_RELEASE = 0x2A;
       BUTTON = "Recall";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Mute") {  // MUTE
+    } else if (inputString == "mute") {  // MUTE
       KEY_CMD = 0xAD;
       KEY_RELEASE = 0x2D;
       BUTTON = "Mute";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Skip_Back") {  // SKIP BACK
+    } else if (inputString == "skip_back") {  // SKIP BACK
       KEY_CMD = 0x9F;
       KEY_RELEASE = 0x1F;
       BUTTON = "Skip_Back";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "REW") {  // REW
+    } else if (inputString == "rew") {  // REW
       KEY_CMD = 0x9F;
       KEY_RELEASE = 0x1F;
       BUTTON = "REW";
       needRelease = 1;
       holdButton = 1;
       triggerMaster();
-    } else if (inputString == "Play_Pause") {  // PAUSE / PLAY
+    } else if (inputString == "play_pause") {  // PAUSE / PLAY
       KEY_CMD = 0xA0;
       KEY_RELEASE = 0x20;
       BUTTON = "Play_Pause";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Skip_Forward") {  // SKIP FORWARD
+    } else if (inputString == "skip_forward") {  // SKIP FORWARD
       KEY_CMD = 0xA1;
       KEY_RELEASE = 0x21;
       BUTTON = "Skip_Forward";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "FFWD") {  // FFWD
+    } else if (inputString == "ffwd") {  // FFWD
       KEY_CMD = 0xA1;
       KEY_RELEASE = 0x21;
       BUTTON = "FFWD";
       needRelease = 1;
       holdButton = 1;
       triggerMaster();
-    } else if (inputString == "Back") {  // BACK
+    } else if (inputString == "back") {  // BACK
       KEY_CMD = 0xA2;
       KEY_RELEASE = 0x22;
       BUTTON = "Back";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Live") {  // LIVE
+    } else if (inputString == "live") {  // LIVE
       KEY_CMD = 0xA2;
       KEY_RELEASE = 0x22;
       BUTTON = "Live";
       needRelease = 1;
       holdButton = 1;
       triggerMaster();
-    } else if (inputString == "Info") {  // INFO
+    } else if (inputString == "info") {  // INFO
       KEY_CMD = 0xA4;
       KEY_RELEASE = 0x24;
       BUTTON = "Info";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Help") {  // HELP
+    } else if (inputString == "help") {  // HELP
       KEY_CMD = 0xA4;
       KEY_RELEASE = 0x24;
       BUTTON = "Help";
       needRelease = 1;
       holdButton = 1;
       triggerMaster();
-    } else if (inputString == "Up") {  // UP
+    } else if (inputString == "up") {  // UP
       KEY_CMD = 0x96;
       KEY_RELEASE = 0x16;
       BUTTON = "Up";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Down") {  //DOWN
+    } else if (inputString == "down") {  //DOWN
       KEY_CMD = 0xA3;
       KEY_RELEASE = 0x23;
       BUTTON = "Down";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Left") {  // LEFT
+    } else if (inputString == "left") {  // LEFT
       KEY_CMD = 0x98;
       KEY_RELEASE = 0x18;
       BUTTON = "Left";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Right") {  // RIGHT
+    } else if (inputString == "right") {  // RIGHT
       KEY_CMD = 0x9A;
       KEY_RELEASE = 0x1A;
       BUTTON = "Right";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Select") {  // SELECT
+    } else if (inputString == "select") {  // SELECT
       KEY_CMD = 0x99;
       KEY_RELEASE = 0x19;
       BUTTON = "Select";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Options") {  // OPTIONS
+    } else if (inputString == "options") {  // OPTIONS
       KEY_CMD = 0x95;
       KEY_RELEASE = 0x15;
       BUTTON = "Options";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Voice") {  // VOICE BUTTON
+    } else if (inputString == "voice") {  // VOICE BUTTON
       KEY_CMD = 0x97;
       KEY_RELEASE = 0x17;
       BUTTON = "Voice";
       needRelease = 1;
       longholdButton = 1;
       triggerMaster();
-    } else if (inputString == "DVR") {  // DVR
+    } else if (inputString == "dvr") {  // DVR
       KEY_CMD = 0x8B;
       KEY_RELEASE = 0x0B;
       BUTTON = "DVR";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Home") {  // HOME
+    } else if (inputString == "home") {  // HOME
       KEY_CMD = 0x8C;
       KEY_RELEASE = 0x0C;
       BUTTON = "Home";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Guide") {  // guide
+    } else if (inputString == "guide") {  // guide
       KEY_CMD = 0x83;
       KEY_RELEASE = 0x03;
       BUTTON = "Guide";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "SAT") {
+    } else if (inputString == "sat") {
       KEY_CMD = 0xEF;
       KEY_RELEASE = 0x6F;
       BUTTON = "SAT";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "TV") {
+    } else if (inputString == "tv") {
       KEY_CMD = 0xF0;
       KEY_RELEASE = 0x70;
       BUTTON = "TV";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "AUX") {
+    } else if (inputString == "aux") {
       KEY_CMD = 0xF1;
       KEY_RELEASE = 0x71;
       BUTTON = "AUX";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Input") {
+    } else if (inputString == "input") {
       KEY_CMD = 0xF2;
       KEY_RELEASE = 0x72;
       BUTTON = "Input";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Power") {
+    } else if (inputString == "power") {
       KEY_CMD = 0x81;
       KEY_RELEASE = 0x01;
       BUTTON = "Power";
       needRelease = 1;
       triggerMaster();
-    } else if (inputString == "Unpair") {  //UNPAIR REMOTE
+    } else if (inputString == "unpair") {  //UNPAIR REMOTE
       KEY_CMD = 0xEF;                      //SAT button
       KEY_RELEASE = 0x6F;
       BUTTON = "SAT";
@@ -359,15 +379,15 @@ void loop() {
       needRelease = 0;
       triggerMaster();
       requestEvent();
-      delay(50);
+      delay(5);
       KEY_CMD = 0x03;  //GUIDE release CMD
       needRelease = 0;
       triggerMaster();
       requestEvent();
-    } else if (inputString == "Debug") {
+    } else if (inputString == "debug") {
       debug = 1;
       Serial.println("debug ON");
-    } else if (inputString == "Debug_off") {  //
+    } else if (inputString == "debug_off") {  //
       debug = 0;
       Serial.println("debug OFF");
     }  
@@ -489,20 +509,21 @@ void triggerMaster() {
   digitalWrite(TRIGGER_PIN, LOW);
   delay(remoteTiming);  // Trigger duration: 5ms
   digitalWrite(TRIGGER_PIN, HIGH);
+  delay(small_delay);
 }
 void reset_mux() {
   Serial.println("resetting MUX");
   digitalWrite(mux_reset, LOW);
-  delay(1);
+  delay(5);
   digitalWrite(mux_reset, HIGH);
-  delay(0);
+  delay(small_delay);
 }
 void reset_rf4ce() {
   Serial.println("Resetting all remotes");
   digitalWrite(rf4ce_reset, LOW);
-  delay(1);
+  delay(5);
   digitalWrite(rf4ce_reset, HIGH);
-  delay(0);
+  delay(small_delay);
 }
 
 void selectDefaultBus() {
@@ -513,10 +534,11 @@ void selectDefaultBus() {
 
 void selectPort(uint8_t pcaAddress, uint8_t port) {
   Serial.print("MUX address: ");
-  Serial.println(pcaAddress, HEX);
-  Serial.print("port: ");
+  Serial.print(pcaAddress, HEX);
+  Serial.print("   port: ");
   Serial.println(port);
   Wire.end();
+  initFlag = 1;
   reset_mux();
   reset_rf4ce();
   delay(10);
@@ -532,7 +554,9 @@ void selectPort(uint8_t pcaAddress, uint8_t port) {
   delay(10);
   //triggerMaster();
   Wire.begin(ARDUINO_SLAVE_ADDR);
-  Serial.print(ARDUINO_SLAVE_ADDR);
+  delay(100);
+
+  //Serial.print(ARDUINO_SLAVE_ADDR);
 }
 void sendrelease() {
   if (holdButton == 1) {
@@ -544,9 +568,7 @@ void sendrelease() {
   }
   delay(90);
   needRelease = 0;
-  if (debug) {
-    Serial.print("Sending release\n");
-  }
+  if (debug) {Serial.println("Sending release"); }
   KEY_CMD = KEY_RELEASE;
   Serial.print("Key release time: ");
   Serial.println(KEY_RELEASE_TIME);
@@ -557,26 +579,30 @@ void sendrelease() {
 void requestEvent(){
   if(readMode == 1) {
 
-    Wire.write(CFG_REG); // 1
-    Wire.write(INT_REG); // 2
-    Wire.write(EVT_REG); // 3
-    Wire.write(KEY_CMD); // 4
-    Wire.write(BLANK);   //5
-    Wire.write(BLANK);    //6
-    Wire.write(BLANK);   // 7
-    Wire.write(BLANK);      //  8
-    Wire.write(BLANK);        //9
-    Wire.write(BLANK);        //10
-    Wire.write(BLANK);        //11
-    Wire.write(BLANK);        //12
-    Wire.write(BLANK);        //13
-    Wire.write(BLANK);//14
-    Serial.println("COMMAND SENT"); 
+    Wire.write(CFG_REG);
+    Wire.read();    
+    Wire.write(INT_REG);
+    Wire.read();    
+    Wire.write(EVT_REG);
+    Wire.read();    
+    Wire.write(KEY_CMD);
+    Wire.read();    
+    Wire.write(BLANK);
+    Wire.write(BLANK);    
+    Wire.write(BLANK);
+    Wire.write(BLANK);        
+    Wire.write(BLANK);        
+    Wire.write(BLANK);        
+    Wire.write(BLANK);        
+    Wire.write(BLANK);        
+    Wire.write(BLANK);        
+    Wire.write(BLANK);
+    Serial.print("COMMAND SENT  "); 
     if (debug){Serial.println(KEY_CMD);}
-    delay(100);  //was 100
+    //delay(100);  //was 100
     }
   else if(readMode == 128) {
-    Serial.println("GOT 0x80/n"); 
+    Serial.println("GOT 0x80"); 
     }  
   else {
     if (debug){Serial.print("GOT DIFFERENT READ MODE  "); }
@@ -584,82 +610,69 @@ void requestEvent(){
     //
     Serial.flush();
     }  
-   //return;
+   return;
   //delay(100);
 }
 
 void receiveEvent(){
-  if (debug) {
-    Serial.println("receive event");
-  }
-
+  if (debug){Serial.println("receive event");}
+  byte byteCount = 0;
   byte byteCursor = 0;
   byte receivedValues[45];
   byte receivedByte = 0;
   byte command = 0;
   byte byteRead = 0;
   receivedValue = 0;
-
-  while (Wire.available()) {
+  while(0 < Wire.available()) // loop through all but the last
+  {
     byteRead = Wire.read();
     
-    if (byteCursor == 0) {
+    if(byteCount ==0) {
       readMode = byteRead;
       command = byteRead;
-      Serial.print("read mode line 607");
-      Serial.println(readMode);
-      Serial.print("COMMAND I GOT: ");
-      Serial.println(command);
+      if (debug){Serial.print("COMMAND I GOT: ");}
+      if (debug){Serial.println(command);}      
     } else {
       receivedByte = byteRead;
-      Serial.print("got more than a command: ");
+      if (debug){Serial.print("got more than a command: ");}
       receivedValues[byteCursor] = receivedByte;
-      Serial.print("receivedByte: ");
-      Serial.println(receivedByte);
+      if (debug){Serial.println(receivedByte);}
+      byteCursor++;
     }
-    byteCursor++;
-    Serial.println("line 617");
-    Serial.print(" byteCursor: "  );
-    Serial.println(byteCursor);
-    Serial.print(" receivedByte: "  );
-    Serial.println(receivedByte);
-    Serial.print(" command: "  );
-    Serial.println(command);
-    Serial.print(" byteRead: "  );
-    Serial.println(byteRead);
-    Serial.print("receivedValue: "  );
-    Serial.println(receivedValue);
+    byteCount++;
+    
+  }
+  for(byte otherByteCursor = byteCursor; otherByteCursor>0; otherByteCursor--) {
+    receivedValue = receivedValue + receivedValues[otherByteCursor-1] * pow(256, byteCursor-1)  ;
+    //Serial.println("qoot: ");
+    if (debug){Serial.print(byteCursor-1);}
+    if (debug){Serial.print(":");}
+    if (debug){Serial.println(receivedValue);}
   }
 
-  for (byte otherByteCursor = byteCursor; otherByteCursor > 1; otherByteCursor--) {
-    receivedValue += receivedValues[otherByteCursor - 2] * pow(256, otherByteCursor - 2);
-    if (debug) {
-      Serial.print(otherByteCursor - 2);
-      Serial.print(":");
-      Serial.println(receivedValue);
-    }
-  }
 
-  if (command == 1) { 
-    if (debug) {
-      Serial.println("COMMAND 1 RECEIVED");
-    }
-    if (initFlag) {
-      CFG_REG = receivedValues[0];
+  if(command == 1) { 
+    if (debug){Serial.println("COMMAND 1 RECEIVED");}
+    if (initFlag){      // allows setting of initFlag once during remote boot  
+      CFG_REG = receivedByte;
       initFlag = 0;
-    }  
+      }  
     return;
   } 
 
-  if (command == 2) {
-    if (debug) {
-      Serial.println("COMMAND 2 RECEIVED");
-      Serial.println("clear interrupt");
-    }
-    byteRead = Wire.read();
-    Serial.print("byteread: ");
+
+  if(command == 2) { //command from microcontroller to clear keypress interrupt
+    if (debug){Serial.println("COMMAND 2 RECEIVED");}
+    if (debug){Serial.println("clear interrupt");}
+    //delay(200);
+    byteRead = Wire.read();   //read one byte  / 0xFF for 54.3 / 0x00 for 54.1
+    Serial.print(" //read one byte  / 0xFF for 54.3 / 0x00 for 54.1   ");
     Serial.println(byteRead);
-    Wire.write(0x00);
-    return;
+    Wire.write(0xFF);    //  0x00 for response back to microcontroller
+    //Serial.flush();
+   return;
+ 
+  }  else { 
+   return;
   }
 }
